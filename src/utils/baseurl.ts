03fe4cae3eb3825f5cf754df2f3505aacbdba7f3
TomOwen93 <mr.tsowen@gmail.com>
2023-08-21 18:02:47 +0100

    update baseurl backend

--- src/utils/baseurl.ts
+++ src/utils/baseurl.ts
@@ -1,4 +1,4 @@
 export const baseUrl =
     process.env.NODE_ENV === "development"
         ? "http://localhost:5000"
-        : "https://tom-todo-app-y14l.onrender.com";
+        : "https://tom-musicshare-app.onrender.com";
